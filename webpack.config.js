8b9b0c94e54aa1e31beba8094e9ff6afd3a49590
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2017-11-08 15:43:20 +0530

    This is the first commit with sample hello message.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var config = {
+		entry: './main.js',
+		output: {
+			filename:'bundle.js',
+		},
+		devServer: {
+			inline: true,
+			port:8080
+		},
+		module : {
+			loaders :[
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+
+					query: {
+						presets: ['es2015','react']
+					}
+				}
+			]
+		}
+	}
+
+	module.exports = config;
